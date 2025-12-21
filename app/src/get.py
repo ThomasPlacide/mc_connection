3f167f519821c95a42d23a9f359fac83de812dc0
thmsplcd <thomasplacide.7@gmail.com>
2025-12-21 12:39:19 +0100

    fixing dict initialization

--- app/src/get.py
+++ app/src/get.py
@@ -13,7 +13,7 @@ def process_mcco_get():
     with open(STATUS_FILE, 'r+') as f:
             status_file = json.load(f)
     
-    worlds_summary = {'worlds': {"world_ID": { "connected": 0, "last_update": "" }}}
+    worlds_summary = {'worlds': {}}
     for world_id, world_data in status_file.get('worlds', {}).items():
         connected = len(world_data.get("IDs_connected", {}))
         last_update = world_data.get("last_update", "")
